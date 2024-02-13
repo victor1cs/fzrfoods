138d06b5225c36c23a81e33fc3185c156fd03069
Victor Correa <victor1cs@outlook.com>
2024-02-13 12:20:11 -0300

    Editando algumas views (apanhando bastante)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # get "welcome/index"
   resources :products
   resources :categories
+  root to: "welcome#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe(Product, type: :model) do
   context "associations" do
     it { should belong_to(:category).optional }
   end
-  
+
   context "validations" do
     it { should validate_presence_of(:name) }
   end
